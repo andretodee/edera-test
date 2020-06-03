9ebea8c1e48f0191ce18fa58a363a8d6ef5f7467
andretodee <andretodee@gmail.com>
2020-06-03 20:37:20 +0200

    finished-day-2

---
+++ script.js
@@ -0,0 +1,91 @@
+// FUNCTION FOR SLIDER USING SLICKJS
+function slider(){
+    $('.slider').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  appendArrows: $('.carosel-arrow'),
+  prevArrow:"<i class='fas fa-long-arrow-alt-left slick-arrow'></i>",
+  nextArrow:"<i class='fas fa-long-arrow-alt-right slick-arrow active'></i>",
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+};
+
+// FUNCTION TO CHANGE THE ARROW AFTER SCROLL
+function arrowScroll(){
+  $(window).on('scroll', function(e) {
+        st = $(this).scrollTop();
+        if (st > 100) {
+            $('.q-arrow').removeClass('active');
+            $('.fa-long-arrow-alt-down').addClass('active');
+        } else {
+            $('.q-arrow').removeClass('active');
+            $('.fa-long-arrow-alt-up').addClass('active');
+        }
+  });
+};
+
+// FUNCTION TO CHANGE THE ACTIVE CLASS ON CLICK
+function tabActive(){
+    $('.menu-it').click(function(){
+        var selectedTab = $(this);
+        $('.menu-it').removeClass('active');
+        selectedTab.addClass('active');
+    });
+};
+
+// FUNCTION TO CHANGE THE ACTIVE CLASS ON CLICK
+function arrowActive(){
+    $('.slick-arrow').click(function(){
+        var selectedArrow = $(this);
+        $('.slick-arrow').removeClass('active');
+        selectedArrow.addClass('active');
+    });
+};
+
+// FUNCTION TO OPEN AND CLOSE THE MENU FOR MOBILE
+function desktopMenu(){
+  $('.mat').click(function(){
+      $('.drop-menu-desktop-wrapper').slideToggle();
+  });
+};
+
+// FUNCTION TO CHANGE THE OPEN CLASS ON CLICK
+function hamburgerOpen(){
+    $('.hamburger-menu').click(function(){
+        $('.h-line-one').toggleClass('open');
+        $('.h-line-two').toggleClass('open');
+    });
+};
+
+// FUNCTION THAT CALLS ALL OTHERS FUNCTIONS
+function init(){
+    slider();
+    desktopMenu();
+    arrowScroll();
+    tabActive();
+    arrowActive();
+    hamburgerOpen();
+};
+
+// INIT FUNCTION CALLED INTO LOADED DOM
+$(document).ready(init);
